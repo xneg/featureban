84b59e8febf07e24b82db4abe234fba1e0ef4379
Andrew <Andrew25455@mail.ru>
2018-09-06 14:53:32 +0300

    Добавили тесты на проверку декремента у TokensPull

---
+++ tests/Featureban.Domain.Tests/TokensPullTests.cs
@@ -0,0 +1,30 @@
+﻿using Featureban.Domain.Tests.DSL;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace Featureban.Domain.Tests
+{
+  public  class TokensPullTests
+    {
+        [Fact]
+        public void LoseToken_WhenDecrement()
+        {
+            var tokensPull = Create.TokenPull().With(1.Token()).Please();
+
+            tokensPull.DecrementToken();
+
+            Assert.False(tokensPull.ContainsTokens);
+        }
+        [Fact]
+        public void DoNothing_WhenDecrementWithoutTokens()
+        {
+            var tokensPull = Create.TokenPull().Please();
+
+            tokensPull.DecrementToken();
+
+            Assert.False(tokensPull.ContainsTokens);
+        }
+    }
+}
